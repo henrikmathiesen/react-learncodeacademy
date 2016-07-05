dbfcf78849540b374cca901187b6f41e3f7681b9
Henrik <henrik_mathiesen@hotmail.com>
2016-07-05 13:05:43 +0200

    -labbing with state

--- 01-basic-react/src/components/Layout.js
+++ 01-basic-react/src/components/Layout.js
@@ -8,6 +8,14 @@ export default class Layout extends React.Component {
     constructor(){
         super(); // must call super here
         this.foo = "bar";
+
+        // state is one way of handling data set it inital in the constructor, when state changes then the DOM will update
+        // use state instead of variables scattered about
+        // use state if it only affects the particular element and is not shared
+        // if something is shared, use props instead
+        this.state = {
+            name: "Henke"
+        };
     }
 
     getVal(n){
@@ -17,6 +25,11 @@ export default class Layout extends React.Component {
     render() {
         const name = "Adam";
         var items = [<Item key="1" />, <Item key="2" />, <Item key="3" />];             // key is good practice says docs https://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+
+        setTimeout(() => {
+            this.setState({ name: "Benke" });                                           // set state
+        }, 2000);
+
         return (
             <div>
                 <Header/>
@@ -25,6 +38,9 @@ export default class Layout extends React.Component {
                 </div>
                 <div>
                     {this.getVal(2)}
+                </div>
+                <div>
+                    {this.state.name}
                 </div>
                 <Footer/>
             </div>
