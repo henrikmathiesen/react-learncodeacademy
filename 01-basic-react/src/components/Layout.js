1d3ece0111869a6fe253950984dde878c3c9180c
Henrik <henrik_mathiesen@hotmail.com>
2016-07-05 11:37:44 +0200

    -labbing on

--- 01-basic-react/src/components/Layout.js
+++ 01-basic-react/src/components/Layout.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Header from './Header';
+import Footer from './Footer'; 
+import Item from './Item';
+
 export default class Layout extends React.Component {
     constructor(){
         super(); // must call super here
@@ -7,18 +11,22 @@ export default class Layout extends React.Component {
     }
 
     getVal(n){
-        return n + 2; 
+        return n + 2;
     }
 
     render() {
         const name = "Adam";
+        var items = [<Item key="1" />, <Item key="2" />, <Item key="3" />];             // key is good practice says docs https://facebook.github.io/react/docs/multiple-components.html#dynamic-children
         return (
             <div>
-                <h2>It is working!</h2>
-                <h2>It is working!</h2>
-                <h2>{name} {3+2}</h2>
-                <h2>{ this.getVal(2) }</h2>
-                <h2>{this.foo}</h2>
+                <Header/>
+                <div>
+                    {items}
+                </div>
+                <div>
+                    {this.getVal(2)}
+                </div>
+                <Footer/>
             </div>
         );
     }
