fb6306b7c409a25efc5bdae9482509d21c9cf77a
Henrik <henrik_mathiesen@hotmail.com>
2016-07-05 14:45:22 +0200

    -labbing on

--- 01-basic-react/src/components/Header.js
+++ 01-basic-react/src/components/Header.js
@@ -3,9 +3,16 @@ import React from 'react';
 import Title from './Header/Title';
 
 export default class Header extends React.Component {
+    handleChange(e){
+        this.props.changeTitle(e.target.value);
+    }
+
     render() {
         return (
-            <header><Title title={this.props.title}/></header>
+            <header>
+                <Title title={this.props.title}/>
+                <div><input onChange={this.handleChange.bind(this)} /></div>
+            </header>
         );
     }
 }
--- 01-basic-react/src/components/Layout.js
+++ 01-basic-react/src/components/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import Header from './Header';
-import Footer from './Footer'; 
+import Footer from './Footer';
 import Item from './Item';
 
 export default class Layout extends React.Component {
-    constructor(){
+    constructor() {
         super(); // must call super here
         this.foo = "bar";
 
@@ -18,7 +18,11 @@ export default class Layout extends React.Component {
         };
     }
 
-    getVal(n){
+    changeTitle(name) {                                                                // We inject this to header and bind this so it gets called in this context
+        this.setState({ name });                                                       // es6 shorthand for { name:name }
+    }
+
+    getVal(n) {
         return n + 2;
     }
 
@@ -26,20 +30,20 @@ export default class Layout extends React.Component {
         const name = "Adam";
         var items = [<Item key="1" />, <Item key="2" />, <Item key="3" />];             // key is good practice says docs https://facebook.github.io/react/docs/multiple-components.html#dynamic-children
 
-        setTimeout(() => {
-            this.setState({ name: "Benke" });                                           // set state
-        }, 2000);
+        // setTimeout(() => {
+        //     this.setState({ name: "Benke" });                                        // set state, this seem to conflict with state change in input box
+        // }, 2000);
 
         return (                                                                        // inject a prop into header elements, Header then passes them through to Title
             <div>
-                <Header title={name} />
+                <Header changeTitle={this.changeTitle.bind(this)} title={name} />
                 <Header title={"Bertil"} />
                 <Header title={this.state.name} />
                 <div>
                     {items}
                 </div>
                 <div>
-                    {this.getVal(2)}
+                    {this.getVal(2) }
                 </div>
                 <div>
                     {this.state.name}
