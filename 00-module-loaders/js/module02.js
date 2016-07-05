4714bd90347e7a11aee356b14fd767b998c35c31
Henrik <henrik_mathiesen@hotmail.com>
2016-07-05 07:14:50 +0200

    -git ignore

--- 00-module-loaders/js/module02.js
+++ 00-module-loaders/js/module02.js
@@ -1,4 +1,7 @@
-console.log("Module 02");
+console.log("Module 02!");
 
 // It is not global and that is very nice
 //console.log(isThisGlobal);
+
+// Can still get to window
+//console.log(window);
