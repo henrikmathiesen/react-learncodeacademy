899963f3abf1b9f13d7f7e2c696ae9a061a21054
Henrik <henrik_mathiesen@hotmail.com>
2016-07-06 18:00:27 +0200

    -active class for sub routes

--- 02-react-router/src/components/Header.js
+++ 02-react-router/src/components/Header.js
@@ -4,25 +4,25 @@ import { Link }                 from 'react-router';
 export default class Layout extends React.Component {
     goBack() {
         //console.log(this.props.routeArgs);
-        this.props.routeArgs.history.goBack();                      // deprecated but we use it for now
-        //this.props.routeArgs.history.pushState(null, "/");        // deprecated but we use it for now
-        //this.props.routeArgs.history.replaceState(null, "/");     // creates no history, deprecated but we use it for now
+        this.props.routeArgs.history.goBack();                                      // deprecated but we use it for now
+        //this.props.routeArgs.history.pushState(null, "/");                        // deprecated but we use it for now
+        //this.props.routeArgs.history.replaceState(null, "/");                     // creates no history, deprecated but we use it for now
     }
 
     render() {
         const {history} = this.props.routeArgs;
         const {location} = this.props.routeArgs;
 
-        const archivesIsActive = location.pathname.match(/^\/archives/) !== null;
+        const archivesIsActive = location.pathname.match(/^\/archives/) !== null;   // matches subroutes also, which activeClassName does not
         console.log("history.isActive: " + history.isActive('archives'));
         console.log("archivesIsActive: " + archivesIsActive);
 
         return (
             <div class="col-sm-12">
                 <h1>Henry News</h1>
-                <Link to="archives" activeClassName="test">Archives</Link>
+                <Link to="archives" class={archivesIsActive ? 'active' : ''}>Archives</Link>
                 <span>&nbsp; </span>
-                <Link to="settings" activeClassName="test"><button class="btn btn-success">Settings</button></Link>
+                <Link to="settings" activeClassName="active"><button class="btn btn-success">Settings</button></Link>
                 <span>&nbsp; </span>
                 <button onClick={this.goBack.bind(this) }>Back</button>
             </div>
