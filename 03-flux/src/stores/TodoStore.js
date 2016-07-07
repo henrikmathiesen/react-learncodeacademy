dfdadf100c509d62382bf8cf755071df8afc405e
Henrik <henrik_mathiesen@hotmail.com>
2016-07-07 16:50:07 +0200

    -labbing on

--- 03-flux/src/stores/TodoStore.js
+++ 03-flux/src/stores/TodoStore.js
@@ -1,4 +1,5 @@
-import { EventEmitter } from 'events';
+import { EventEmitter } from 'events'; // this comes nativly from node
+import dispatcher from '../Dispatcher';
 
 export default class TodoStore extends EventEmitter {
     constructor() {
@@ -40,10 +41,28 @@ export default class TodoStore extends EventEmitter {
 
         this.emit('change');
     }
+
+    handleActions(action) {
+        console.log("TodoStore recieved an action");
+        console.log(action);
+        console.log("/TodoStore recieved an action");
+
+        switch(action.type) {
+            case "CREATE_TODO":
+                this.createTodo(action.text);
+                break;
+        }
+    }
 }
 
 const todoStore = new TodoStore();
-//window.todoStore = todoStore; // debug for creating todos in console
+
+dispatcher.register(todoStore.handleActions.bind(todoStore));
+
+// debug for creating todos in console
+//window.todoStore = todoStore;
+window.dispatcher = dispatcher;
+
 export default todoStore;
 
 // Why create one instance and return it when imported? Wouldnt static members be better?
